ed8de948512468c2dc828f291705d07c0e7e7d8d
Brandon Shankar <bshankar12@comcast.net>
2019-10-27 21:42:24 -0700

    added timer function to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,11 @@
-//create variables needed 
-var 
-//create a function to start the game
-function startGame()    {
-
+//intiate the program
+$(document).ready(function()   {
+    //create the timer for the game
+    var timerCounter = 30;
+    var triviaTimer = setInterval(function()    {
+        timerCount--;
+        document.getElementById("timer").textContent = timerCounter;
+        if (timerCounter <= 0)
+        clearInterval(triviaTimer);
+    }, 1000)
 }
