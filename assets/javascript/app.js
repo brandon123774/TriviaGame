ad8d873bf27e59020199d6534909ba293d26ba40
Brandon Shankar <bshankar12@comcast.net>
2019-10-27 21:55:19 -0700

    added question/ answer loops to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,31 @@
 //intiate the program
-$(document).ready(function()   {
+$(document).ready(function () {
     //create the timer for the game
     var timerCounter = 30;
-    var triviaTimer = setInterval(function()    {
+    var triviaTimer = setInterval(function () {
         timerCount--;
         document.getElementById("timer").textContent = timerCounter;
         if (timerCounter <= 0)
-        clearInterval(triviaTimer);
+            clearInterval(triviaTimer);
     }, 1000)
-}
+    // create the trivia board
+    function triviaGame() {
+        //stores the HTML outputs to this array
+        var output = [];
+
+        //questions need to loop 
+        triviaQ.forEach(newQ, qNum) => {
+            var answerBox = []
+            for (answerChoice in newQ.answerBox) {
+                answerBox.push(
+                    <label>
+                        <input type='radio' name="question$(qNum)" value="$(answerChoice)">
+                            ${answerChoice} :
+              ${newQ.answerBox[answerChoice]}
+                </label>
+                        );
+                    }
+                });
+                
+            }
+            }
